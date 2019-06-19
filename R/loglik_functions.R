b5292519dba3742fc6acafc8ba5bf616b5666c2f
Kim Rand <intelligentaccident@gmail.com>
2019-06-19 16:16:17 +0200

    Added random-intercept logistic function

--- R/loglik_functions.R
+++ R/loglik_functions.R
@@ -120,3 +120,57 @@ dich_normal <- function(d_df, theta_est = 0) {
                                  start_values = c(THETA = 1)))
   return(d_df$internal_lb*pnorm(d_df$Xb/d_df$theta_est, 0, 1, log.p = TRUE) + (1-d_df$internal_lb)*pnorm(-d_df$Xb/d_df$theta_est, 0, 1, log.p = TRUE))
 }
+
+
+#' Likelihood functions for data.frame
+#' @title Logistic-distribution-based likelihood function for dichotomous data with random intercepts, used by xregControl and xreg
+#' @param d_df data.frame provided by xreg.
+#' @param theta_est Subsequent parameters define required formulas to genrete necessary variables, such as standard deviations. If these are not manually provided (which could be useful to model heteroscedasticity, etc.), xreg will get them from the functions.
+#' @param sigma_est Within-unit variance parameter, may be specified as a function to vary between observations. Defaults to exp(LN_SIGMA)
+#' @param omega_est Between-unit variance parameter, must be a fixed result for all observations, as only the first instance will be used. Defaults to exp(LN_OMEGA)
+#' @description Likelihood function to use with xregControl
+#' @export
+dich_r_logistic <- function(d_df, theta_est = 0, omega_est = 0, internal_id = 0) {
+  if(theta_est == 1) return(list(formula = formula(theta_est ~ THETA, env = globalenv()),
+                                 start_values = c(THETA = 1)))
+  if (internal_id == 1)
+    return(list(formula = formula(internal_id ~ id, env = globalenv())))
+  if (omega_est == 1)
+    return(list(formula = formula(omega_est ~ exp(LN_OMEGA), env = globalenv()),
+                start_values = c(LN_OMEGA = 0)))
+  if (min(d_df$omega_est) <= 0)
+    return(NA)
+  
+
+  betwSD <- d_df$omega_est[1]
+  
+  uid <- sort(unique(d_df$internal_id))
+  q <- NROW(uid)
+  
+  d_df$p <- 0
+  
+  order <- 20L
+  
+  ws <- as.matrix(xreg_GHS[[order]]$w)
+  xs <- xreg_GHS[[order]]$x
+  mus <- sqrt(2) * betwSD * xs
+  
+  tmpm <- matrix(NA, nrow = NROW(d_df), ncol = order)
+  
+  
+  
+  tmpm <- d_df$internal_lb*(-log(1+exp(-(tmpxb<-outer(d_df$Xb, mus, FUN = "+"))*d_df$theta_est)))   + (1-d_df$internal_lb)*(-tmpxb*d_df$theta_est   - log(1+exp(-tmpxb*d_df$theta_est)))
+  
+  
+  
+  
+  d_df$p[match(uid, d_df$internal_id)] <- log(1/sqrt(pi)*as.matrix(aggregate(tmpm, by = list(d_df$internal_id), FUN = prod)[,-1]) %*% ws)
+  d_df$p[d_df$p == -Inf] <- log(.Machine$double.xmin)
+  
+  
+  return(d_df$p)
+  
+    
+}
+
+
